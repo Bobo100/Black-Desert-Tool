d1d69ba5e92fbaa3bdeebf27d22f2a1b4b33f7ba
Bobo100 <w150lione@gmail.com>
2023-02-24 12:12:05 +0800

    更新伺服器檔案

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,11 +3,6 @@ import Search from './component/search';
 import Table from './component/table';
 import TableExcel from './component/table_excel';
 
-import { google } from 'googleapis';
-import { JWT } from 'google-auth-library';
-
-import key from "../lib/black-desert-378517-94f7666d54d8.json"
-
 const data = [
     ['apple', 'red', 'fruit'],
     ['banana', 'yellow', 'fruit'],
@@ -22,21 +17,14 @@ export const Home = () => {
     const handleSearch = (value: string) => {
         setKeyword(value);
     };
-
-    // 建立 JWT 客戶端
-    // const client = new JWT({
-    //     email: key.client_email,
-    //     key: key.private_key,
-    //     scopes: ['https://www.googleapis.com/auth/spreadsheets'],
-    // });
-
-
+    
     return (
         <div>
             <Search value={keyword} onChange={handleSearch} />
             <Table data={data} keyword={keyword} />
 
             <TableExcel />
+
         </div>
     );
 };
