394fec9e925b5d60805d4d87f875040090a32d6c
Bobo100 <w150lione@gmail.com>
2023-03-01 02:55:05 +0800

    更新首頁

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ function App() {
 
   return (
     <div className="App">
-      <Router basename="/">
+      <Router basename="/BlackDesertTool">
         <NavBar />
         <div className="pages">
           <Routes>
